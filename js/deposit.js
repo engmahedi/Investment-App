db8b67a0482a3098e1ca0dd4c2ac5dbb3596237a
Mahedi <faruk585@gmail.com>
2023-08-22 11:37:16 +0600

    total amount add

--- js/deposit.js
+++ js/deposit.js
@@ -5,7 +5,24 @@ depositBtn.addEventListener("click", function () {
   const depositInput = document.getElementById("depositInput").value;
   document.getElementById("depositInput").value = '';
   
-  const depositAmount = document.getElementById("depositAmount").innerText;
-  document.getElementById("depositAmount").innerText=depositInput;
-  console.log(depositAmount);
+  if (depositInput >= 100) {
+    // Total Deposite Display
+
+    const depositAmount = document.getElementById("depositAmount").innerText;
+    const totalDeposit = Math.floor(depositAmount) + Math.floor(depositInput)
+    document.getElementById("depositAmount").innerText = totalDeposit;
+  
+    // Total Amount Display
+    
+    const balance = document.getElementById("balance").innerText;  
+    const totalBalance = Math.floor(balance) + Math.floor(depositInput);
+    document.getElementById("balance").innerText = totalBalance;
+    
+  swal("Deposit Sucessfull!", "Thank You", "success");
+  } else {
+    swal("Insufficient amount!", "Minimum 100 TK", "error");
+  }
+
+  
+  
 })
--- js/withdrow.js
+++ js/withdrow.js
@@ -6,8 +6,24 @@ withdrowBtn.addEventListener("click", function () {
   const withdrowInput = document.getElementById("withdrowInput").value;
   document.getElementById("withdrowInput").value = '';
 
-  const withdrowAmount = document.getElementById("withdrowAmount").innerText;
+  if (withdrowInput >= 100) {
+    
+    // Total Withdraw Display
+    
+    const withdrowAmount = document.getElementById("withdrowAmount").innerText;
+    const totalWithdraw=Math.floor(withdrowAmount)+Math.floor(withdrowInput)
+    document.getElementById("withdrowAmount").innerText = totalWithdraw;
+
+    // Total Amount Display
+
+    const balance = document.getElementById("balance").innerText;
+    const totalBalance = Math.floor(balance) - Math.floor(withdrowInput);
+    document.getElementById("balance").innerText=totalBalance;
+
+    swal("Withdraw Sucessfull!", "Thank You", "success");
+  } else {
+    swal("Insufficient amount!", "Minimum 100 TK", "error");
+  }
+ 
   
-  document.getElementById("withdrowAmount").innerText = withdrowInput;
-  console.log(withdrowAmount);
 })
